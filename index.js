386d517ff0dbf53f069d07d3dc668bf50117c1bf
ankihg <hagelin.annika@gmail.com>
2016-04-09 00:34:50 -0700

    delete species

--- index.js
+++ index.js
@@ -6,19 +6,27 @@ angular.module('TreeApp', [])
   .controller('SpeciessController', ['$http', function($http) {
     this.plz = 'plz respond';
 
+    this.resource = 'speciess'
+    this.path = `${serverPath}/${this.resource}`;
     this.speciess = [];
 
-    this.getSpeciess = function() {
-      $http.get(serverPath+'/speciess')
+    this.read = function() {
+      $http.get(this.path)
         .then(res => this.speciess = res.data)
         .catch(err => console.log(err));
-    }
+    };
 
-    this.createSpecies = function(species) {
-      $http.post(serverPath+'/speciess', species)
+    this.create = function(species) {
+      $http.post(this.path, species)
         .then(res => this.speciess.push(res.data))
         .catch(err => console.log(err));
-    }
+    };
+
+    this.delete = function(species) {
+      $http.delete(this.path+'/'+species._id)
+        .then(res => this.speciess.splice(this.speciess.indexOf(species), 1))
+        .catch(err => console.log(err));
+    };
 
 
   }])
